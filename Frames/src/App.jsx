f1c8dbd23b7a2fc32538d71b9ca8e99f1a925fc6
Adir <slutzkis@gmail.com>
2024-10-10 15:16:23 +0300

    Update App.jsx

--- Frames/src/App.jsx
+++ Frames/src/App.jsx
@@ -2,7 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Frame from './components/Frame';
+// import Frame from './components/Frame';
+import ReactIframe from 'react-iframe';
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,11 +13,16 @@ function App() {
     <>
       <h1>Frames</h1>
       <button>Click me</button>
+
+      <ReactIframe url="https://www.google.com" className='frame' />
+      <ReactIframe url="https://adir.dev" className='frame' />
+      <ReactIframe url="https://github.com/Adir-SL"  className='frame'/>
+      <ReactIframe url="https://adir.dev" className='frame' />
       
-      <Frame source="https://www.google.com" />
+      {/* <Frame source="https://www.google.com" />
       <Frame source="https://adir.dev" />
       <Frame source="https://github.com/Adir-SL" />
-      <Frame source="https://adir.dev" />
+      <Frame source="https://adir.dev" /> */}
     </>
   )
 }
