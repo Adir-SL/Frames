e89039c24c002e8134ae89d55251854c3a359784
Adir <slutzkis@gmail.com>
2024-10-27 14:13:18 +0200

    Update App.jsx

--- Frames/src/App.jsx
+++ Frames/src/App.jsx
@@ -5,18 +5,18 @@ import './App.css'
 import ReactIframe from 'react-iframe';
 import './Frame.css'
 import GitHubCalendar from 'react-github-calendar';
+import React, { useRef } from 'react'
+import { useDrag, useDrop } from 'react-dnd'
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div className="content">
         <h1>Frames</h1>
-        <GitHubCalendar username="adir-sl" className='frame' />
-        <ReactIframe url="https://adir.dev" className='frame' />
-        <ReactIframe url="https://en.wikipedia.org/wiki/MacOS" className='frame' />
+        <GitHubCalendar username="adir-sl" className='frame' data-swapy-slot="a" />
+        <ReactIframe url="https://adir.dev" className='frame' data-swapy-slot="b" />
+        <ReactIframe url="https://en.wikipedia.org/wiki/MacOS" className='frame' data-swapy-slot="c" />
         <ReactIframe url="https://adir.dev" className='frame' />
         <ReactIframe url="https://en.wikipedia.org/wiki/MacOS" className='frame' />
       </div>
