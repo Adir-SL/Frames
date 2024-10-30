86b15dfd1c0cec84d928cbd0246e73092e2cc794
Adir <slutzkis@gmail.com>
2024-10-30 10:04:58 +0200

    updates

--- Frames/src/App.jsx
+++ Frames/src/App.jsx
@@ -3,10 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import ReactIframe from 'react-iframe';
-import './Frames.css'
-import GitHubCalendar from 'react-github-calendar';
-import React, { useRef } from 'react'
-import { useDrag, useDrop } from 'react-dnd'
+import './Frames.css';
 
 
 function App() {
--- Frames/src/components/Github.jsx
+++ Frames/src/components/Github.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
-import './Github.css'
+import GitHubCalendar from 'react-github-calendar';
+import './Github.css';
 
 function Github(props) {
 
