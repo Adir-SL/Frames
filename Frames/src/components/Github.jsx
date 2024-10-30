9506db89dba4321c730d0bdebe563aec632273df
Adir <slutzkis@gmail.com>
2024-10-30 10:12:38 +0200

    updates

--- Frames/src/components/Github.jsx
+++ Frames/src/components/Github.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import GitHubCalendar from 'react-github-calendar';
+import Header from "./Header";
 
 function Github(props) {
 
   return (
-    <>
+    <div className='github-frame'>
+      <Header></Header>
       <GitHubCalendar username={props.user} className='frame' data-swapy-slot="a" />
-    </>
+    </div>
   )
 }
 
---
+++ Frames/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function Header(props) {
+
+  return (
+    <header>
+      <button>^</button>
+      <button>^</button>
+    </header>
+  )
+}
+
+export default Header
